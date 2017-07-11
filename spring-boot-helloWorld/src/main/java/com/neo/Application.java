8da66d00f2be915e8739e15ba812ee98d7ff2e22
huangfu chunfeng <huangfuchunfeng@fangdd.com>
2017-07-11 19:45:59 +0800

    add logback adapter

--- spring-boot-helloWorld/src/main/java/com/neo/Application.java
+++ spring-boot-helloWorld/src/main/java/com/neo/Application.java
@@ -3,12 +3,20 @@ package com.neo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 @EnableScheduling
 public class Application {
-
+	final static Logger logger = LoggerFactory.getLogger("MyApp.class");
+	
 	public static void main(String[] args) {
+		logger.trace("trace");
+		logger.debug("debug str");
+		logger.info("info str");
+		logger.warn("warn");
+		logger.error("error");		
 		SpringApplication.run(Application.class, args);
 	}
 }
